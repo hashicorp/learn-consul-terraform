f7dc390d7dbf75464b715fb822827ba317d2f05a
Eddie Rowe <74205376+eddie-rowe@users.noreply.github.com>
2021-10-14 15:12:28 -0500

    content updates

---
+++ datacenter-deploy-ecs-ec2-existing/modules.tf
@@ -0,0 +1,92 @@
+module "acl_controller" {
+  source     = "../modules/acl-controller"
+  log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = aws_cloudwatch_log_group.log_group.name
+      awslogs-region        = var.region
+      awslogs-stream-prefix = "consul-acl-controller"
+    }
+  }
+  consul_bootstrap_token_secret_arn = aws_secretsmanager_secret.bootstrap_token.arn
+  consul_server_ca_cert_arn         = aws_secretsmanager_secret.consul_ca_cert.arn
+  consul_server_http_addr           = "https://${var.consul_cluster_ip}:8501"
+  ecs_cluster_arn                   = aws_ecs_cluster.this.arn
+  region                            = var.region
+  subnets                           = var.private_subnets_ids
+  name_prefix                       = var.name
+}
+
+module "example_client_app" {
+  source            = "../modules/mesh-task"
+  family            = "${var.name}-example-client-app"
+  port              = "9090"
+  log_configuration = local.example_client_app_log_config
+  container_definitions = [{
+    name             = "example-client-app"
+    image            = "ghcr.io/lkysow/fake-service:v0.21.0"
+    essential        = true
+    logConfiguration = local.example_client_app_log_config
+    environment = [
+      {
+        name  = "NAME"
+        value = "${var.name}-example-client-app"
+      },
+      {
+        name  = "UPSTREAM_URIS"
+        value = "http://localhost:1234"
+      }
+    ]
+    portMappings = [
+      {
+        containerPort = 9090
+        hostPort      = 9090
+        protocol      = "tcp"
+      }
+    ]
+    cpu         = 0
+    mountPoints = []
+    volumesFrom = []
+  }]
+  upstreams = [
+    {
+      destination_name = "${var.name}-example-server-app"
+      local_bind_port  = 1234
+    }
+  ]
+  retry_join                     = var.consul_cluster_ip
+  tls                            = true
+  consul_server_ca_cert_arn      = aws_secretsmanager_secret.consul_ca_cert.arn
+  gossip_key_secret_arn          = aws_secretsmanager_secret.gossip_key.arn
+  acls                           = true
+  consul_client_token_secret_arn = module.acl_controller.client_token_secret_arn
+  acl_secret_name_prefix         = var.name
+  consul_datacenter              = var.consul_datacenter
+}
+
+module "example_server_app" {
+  source            = "../modules/mesh-task"
+  family            = "${var.name}-example-server-app"
+  port              = "9090"
+  log_configuration = local.example_server_app_log_config
+  container_definitions = [{
+    name             = "example-server-app"
+    image            = "ghcr.io/lkysow/fake-service:v0.21.0"
+    essential        = true
+    logConfiguration = local.example_server_app_log_config
+    environment = [
+      {
+        name  = "NAME"
+        value = "${var.name}-example-server-app"
+      }
+    ]
+  }]
+  retry_join                     = var.consul_cluster_ip
+  tls                            = true
+  consul_server_ca_cert_arn      = aws_secretsmanager_secret.consul_ca_cert.arn
+  gossip_key_secret_arn          = aws_secretsmanager_secret.gossip_key.arn
+  acls                           = true
+  consul_client_token_secret_arn = module.acl_controller.client_token_secret_arn
+  acl_secret_name_prefix         = var.name
+  consul_datacenter              = var.consul_datacenter
+}
---
+++ datacenter-deploy-ecs-hcp-existing/variables.tf
@@ -0,0 +1,45 @@
+variable "name" {
+  description = "Name to be used on all the resources as identifier."
+  type        = string
+  default     = "consul-ecs"
+}
+
+variable "region" {
+  description = "AWS region."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS access key."
+  type        = string
+  default     = null
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key."
+  type        = string
+  default     = null
+}
+
+variable "lb_ingress_ip" {
+  description = "Your Public IP. This is used in the load balancer security groups to ensure only you can access the Consul UI and example application."
+  type        = string
+}
+
+
+variable "hcp_client_id" {
+  description = "HCP Client ID."
+  type        = string
+}
+
+variable "hcp_client_secret" {
+  description = "HCP Client Secret."
+  type        = string
+}
+
+variable "hcp_datacenter_name" {
+  type = string
+  description = "The name of datacenter the Consul cluster belongs to"
+  default = "dc1"
+}
