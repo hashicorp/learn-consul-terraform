a343c6edd0df61124123f308276430b9dba6f811
Christian Weber <christian.weber@hashicorp.com>
2022-04-20 11:17:23 -0500

    Removing code that will be copied by the practitioner in the tutorial content

--- datacenter-deploy-ecs-hcp-ap/consul-service_defaults.tf
+++
@@ -1,9 +0,0 @@
-resource "consul_config_entry" "product-api" {
-  kind = "service-defaults"
-  name = data.consul_service.each["product-api"].name
-
-
-  config_json = jsonencode({
-    Protocol = local.consul_service_defaults_protocols.tcp
-  })
-}
--- datacenter-deploy-ecs-hcp-ap/reader_files/reader-consul-admin_partitions.tf
+++
@@ -1,6 +0,0 @@
-resource "consul_admin_partition" "partition-two" {
-  name        = local.admin_partitions.two
-  description = "Admin Partition for public facing HashiCups services"
-
-  depends_on = [hcp_consul_cluster.example]
-}
