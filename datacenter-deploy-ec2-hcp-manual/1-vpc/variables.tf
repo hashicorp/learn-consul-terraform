828e2200cf5bdcb0041c79d6901cc8bffbfa9939
Tu Nguyen <im2nguyen@gmail.com>
2022-05-23 23:41:15 -0700

    create initial template. TODO: rm nomad, nginx, manually set variables in step 3

---
+++ datacenter-deploy-ec2-hcp-manual/1-vpc/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "name" {
+  default = "learn-hcp-consul-ec2"
+}
---
+++ datacenter-deploy-ec2-hcp-manual/2-hcp/outputs.tf
@@ -0,0 +1,16 @@
+output "consul_root_token" {
+  value     = hcp_consul_cluster_root_token.token.secret_id
+  sensitive = true
+}
+
+output "consul_url" {
+  value = hcp_consul_cluster.main.public_endpoint ? (
+    hcp_consul_cluster.main.consul_public_endpoint_url
+    ) : (
+    hcp_consul_cluster.main.consul_private_endpoint_url
+  )
+}
+
+output "hcp_consul_security_group_id" {
+  default = module.aws_hcp_consul.security_group_id
+}
