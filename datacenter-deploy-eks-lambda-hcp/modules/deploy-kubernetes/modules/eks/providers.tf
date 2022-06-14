24b08e264c09d38e3ea0d44fb6743782ad28cd93
Christian Weber <christian.weber@hashicorp.com>
2022-06-14 13:17:43 -0500

    Organize and clarify the usage of values of the providers

--- datacenter-deploy-eks-lambda-hcp/modules/deploy-kubernetes/modules/eks/providers.tf
+++ datacenter-deploy-eks-lambda-hcp/modules/deploy-kubernetes/modules/eks/providers.tf
@@ -7,42 +7,40 @@ terraform {
   }
 }
 
-provider "kubectl" {
-  # Configuration options
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  load_config_file       = false
-  token                  = data.aws_eks_cluster_auth.main.token
-}
-
-
-data "aws_eks_cluster_auth" "main" {
-  name       = var.cluster_name
-  depends_on = [module.eks]
-}
-
 data "aws_eks_cluster" "this" {
   name = module.eks.cluster_id
 }
 
+provider "kubectl" {
+  # Configuration options
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command = "aws"
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id ]
+  }
+}
+
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint #module.eks.cluster_endpoint
+  host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
   }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint #module.eks.cluster_endpoint
+    host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
     }
   }
 }
