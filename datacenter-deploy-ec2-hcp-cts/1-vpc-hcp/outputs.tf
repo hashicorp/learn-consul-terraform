663fd8ac6d57839843c5753f25b0d6842bbffc39
Krastin Krastev <krastin@hashicorp.com>
2022-07-06 18:13:08 +0300

    deploy-ec2-hcp-cts: adding a deployment for using CTS on an EC2 instance with HCP

---
+++ datacenter-deploy-ec2-hcp-cts/1-vpc-hcp/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "AWS VPC ID"
+}
+
+output "vpc_cidr_block" {
+  value       = module.vpc.vpc_cidr_block
+  description = "AWS VPC CIDR block"
+}
+
+output "subnet_id" {
+  value       = module.vpc.public_subnets[0]
+  description = "AWS public subnet"
+}
+
+output "hcp_consul_cluster_id" {
+  value       = hcp_consul_cluster.main.cluster_id
+  description = "HCP Consul ID"
+}
+
+output "hcp_consul_security_group" {
+  value       = module.aws_hcp_consul.security_group_id
+  description = "AWS Security group for HCP Consul"
+}
---
+++ datacenter-deploy-ec2-hcp-cts/2-ec2-consul-client-cts/variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_id" {
+  type        = string
+  description = "AWS VPC ID"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "AWS CIDR block"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "AWS subnet (public)"
+}
+
+variable "cluster_id" {
+  type        = string
+  description = "HCP Consul ID"
+}
+
+variable "hcp_consul_security_group_id" {
+  type        = string
+  description = "AWS Security group for HCP Consul"
+}
+
+variable "cts_version" {
+  type        = string
+  description = "CTS version to install"
+  default     = "0.6.0+ent"
+}
