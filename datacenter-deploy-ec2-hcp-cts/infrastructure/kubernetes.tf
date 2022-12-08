39050fce7e44f0a44b11e35d11db59be3c988204
Krastin Krastev <krastin@hashicorp.com>
2022-12-08 12:35:27 +0100

    Update kubernetes.tf

--- datacenter-deploy-ec2-hcp-cts/infrastructure/kubernetes.tf
+++ datacenter-deploy-ec2-hcp-cts/infrastructure/kubernetes.tf
@@ -1,6 +1,6 @@
 # Kubernetes provider
-# https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
-# To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
+# To learn how to schedule deployments and services using the provider, go here:
+# https://developer.hashicorp.com/terraform/tutorials/kubernetes/kubernetes-provider
 
 # The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
 # You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
